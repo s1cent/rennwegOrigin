548aafa658b5f57101cde3e1ce18899e277fb054
s1cent <44167062+s1cent@users.noreply.github.com>
2022-12-28 14:10:55 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  googleSheetsApiKey: 'AIzaSyCWc3_7ZN0o0HC6PZNNcfs4NXXJ2TmUliQ',
+  test: {
+    spreadsheetId: '1p2LonI1UixhdJNd_5gTis5PwNubRjcWx571vOtTo3Us',
+    worksheetName: 'Test',
+  }
 };
